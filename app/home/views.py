16dc1bb3f5689d1b045f98f3f1226464d77dc958
Suyongzhi1997 <1125699801@qq.com>
2018-12-04 13:53:56 +0800

    代码优化及BUG处理
    Signed-off-by: Suyongzhi1997 <1125699801@qq.com>

--- app/home/views.py
+++ app/home/views.py
@@ -299,6 +299,7 @@ def search(page=None):
     ).order_by(
         Movie.addtime.desc()
     ).paginate(page=page, per_page=10)
+    page_data.key = key
     return render_template("home/search.html", key=key, page_data=page_data, movie_count=movie_count)
 
 
